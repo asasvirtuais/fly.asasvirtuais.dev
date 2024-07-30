d40985801b082efc3b45812af2a415ad10ab7e91
Icaro Asas Virtuais <39817766+asas-virtuais@users.noreply.github.com>
2024-07-30 07:05:16 +0000

    fix tsconfig.json updated

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,5 @@ import { App } from '@tinyhttp/app'
 const app = new App()
 
 app
-  .get('/', (_, res) => void res.send('<h1>Hello World</h1>'))
+  .get('/', (_, res) => void res.send('<h1>Hello again. World!</h1>'))
   .listen(3000, () => console.log('Server is running on http://localhost:3000'))
